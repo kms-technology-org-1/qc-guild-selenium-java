fb3f7256f3df199c9ffa18e19e28a94acda08e13
KMS\namhoang <namhoang@kms-technology.com>
2024-04-14 01:50:01 +0700

    Drivers setup

--- src/test/java/org/kms/com/groupup04/commons/EdgeDriverFactory.java
+++ src/test/java/org/kms/com/groupup04/commons/EdgeDriverFactory.java
@@ -3,12 +3,12 @@ package org.kms.com.groupup04.commons;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.kms.com.groupup04.pageobjects.BasePage;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 
 public class EdgeDriverFactory extends BasePage {
     public static WebDriver initDriver() {
-        WebDriverManager.firefoxdriver().setup();
-        driver = new FirefoxDriver();
+        WebDriverManager.edgedriver().setup();
+        driver = new EdgeDriver();
         return driver;
     }
 }
