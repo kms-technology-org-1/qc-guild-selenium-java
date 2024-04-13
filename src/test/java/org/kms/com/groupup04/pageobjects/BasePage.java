c8a6c3cdc4f7be5b682e560fcedfe420ee6ce0f1
KMS\namhoang <namhoang@kms-technology.com>
2024-04-14 01:47:22 +0700

    Drivers setup

--- src/test/java/org/kms/com/groupup04/pageobjects/BasePage.java
+++ src/test/java/org/kms/com/groupup04/pageobjects/BasePage.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.WebDriver;
 
 public class BasePage {
     public static WebDriver driver;
-    public static  String env = System.getProperty("env") == null ? "qa" : System.getProperty("env");
-    public static  String browser = System.getProperty("browser") == null ? "chrome" : System.getProperty("browser");
+    public static String env = System.getProperty("env") == null ? "qa" : System.getProperty("env");
+    public static String browser = System.getProperty("browser") == null ? "chrome" : System.getProperty("browser");
     public static ConfigReader config = new ConfigReader("src/test/resources/configs/" + env + ".properties");
 }
